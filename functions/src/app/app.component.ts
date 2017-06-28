9667cacf9236d2955e8927a773198449654051ee
sd <sdebaun74@gmail.com>
2017-06-28 00:51:03 -0700

    test case demoing shared code used in ng both JIT and AOT

--- functions/src/app/app.component.ts
+++ functions/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { foo } from '../../shared/foo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = foo();
 }
