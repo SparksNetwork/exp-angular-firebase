3d1ccce8e4022f087d9325cb8d3baa491de11aa6
sd <sdebaun74@gmail.com>
2017-06-28 01:40:00 -0700

    fn serving client w firebase serve and deployed

---
+++ functions/fn/src/client.ts
@@ -0,0 +1,41 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone-node';
+import { platformServer, renderModuleFactory } from '@angular/platform-server'
+import { enableProdMode } from '@angular/core'
+import { AppServerModuleNgFactory } from '../../dist/ngfactory/src/app/app.server.module.ngfactory'
+import * as express from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+enableProdMode();
+
+const app = express();
+
+let template = readFileSync(join(__dirname, '..', '..', '..', '..', 'dist', 'index.html')).toString();
+
+app.engine('html', (_, options, callback) => {
+  const opts = { document: template, url: options.req.url };
+
+  renderModuleFactory(AppServerModuleNgFactory, opts)
+    .then(html => callback(null, html));
+});
+
+app.set('view engine', 'html');
+app.set('views', 'src')
+
+app.get('*.*', express.static(join(__dirname, '..', '..', 'public')));
+
+app.get('*', (req, res) => {
+  res.render('index', { req });
+});
+
+import * as functions from 'firebase-functions'
+
+export const client = functions.https.onRequest((req,res) => {
+  // NOTE: You need to add a trailing slash to the root URL becasue of this issue: https://github.com/firebase/firebase-functions/issues/27
+  // without trailing "/", req.path = null, req.url = null
+  // won't match to your app.get('/', ...) route
+  // open issue: https://github.com/firebase/firebase-functions/issues/27
+  if (!req.path) { req.url = `/${req.url}` }
+  return app(req,res)
+});
---
+++ functions/fn/src/helloWorld.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+import { foo } from '../../shared/foo';
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase! " + foo());
+});
